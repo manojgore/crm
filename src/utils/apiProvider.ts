4ce35f00215006b6ee0f458055f8b65d006da4a8
MANOJ GORE <manojgore.mg@gmail.com>
2024-10-11 11:01:09 +0530

    api provider

--- src/utils/apiProvider.ts
+++ src/utils/apiProvider.ts
@@ -1,2 +1,2 @@
-export const api = "https://server.taxrx.in";
-// export const api = "http://localhost:3001";
+// export const api = "https://server.taxrx.in";
+export const api = "http://localhost:3001";
