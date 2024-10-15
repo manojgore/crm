6cead9b0e3e25c007bd9b190dfa685ed8c930d0a
MANOJ GORE <manojgore.mg@gmail.com>
2024-10-15 14:56:43 +0530

    as

--- src/components/Layouts/SidebarAdmin.tsx
+++ src/components/Layouts/SidebarAdmin.tsx
@@ -105,13 +105,6 @@ const SidebarAdmin = () => {
                                     </button>
                                 </NavLink>
                             </li>
-
-                            {/* {Controll Section} */}
-                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
-                                <IconMinus className="w-4 h-5 flex-none hidden" />
-                                <span>{t('Admin Controll')}</span>
-                            </h2>
-
                             <li className="nav-item">
                                 <ul>
                                     <li className="nav-item">
